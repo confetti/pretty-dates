51a38883b8b7869baf696c24bcbfd0d827ab1b22
Boris Grubesic <31404065+borkanee@users.noreply.github.com>
2021-03-02 13:18:40 +0100

    Updates libs and adds showTimeZone
    * Updates libs and adds showTimeZone
    
    * Test use moment.tz instead to fix tests
    
    * uses moment.tz in all tests
    
    * Fix

--- src/index.js
+++ src/index.js
@@ -1,39 +1,54 @@
-var moment = require('moment-timezone');
+const moment = require('moment-timezone')
 
-var sameDayOrNight = function(start, end) {
-  return (end.isSame(start, 'day') || ((end.diff(start, 'hours') < 24) && end.hours() < 8));
-};
+const sameMonth = (start, end) => end.month() === start.month()
+const sameDayOrNight = (start, end) => end.isSame(start, 'day') || (end.diff(start, 'hours') < 24 && end.hours() < 8)
 
-module.exports = function({endDate, startDate, timeZone, timeFormat, locale}, format) {
-  if (!timeZone) { timeZone = 'Europe/Stockholm'; }
-  if (!timeFormat) { timeFormat = '24'; }
-  if (!locale) { locale = 'en'; }
+module.exports = function ({ endDate, startDate, timeZone, timeFormat, locale, showTimeZone }, format) {
+  if (!timeZone) {
+    timeZone = 'Europe/Stockholm'
+  }
+  if (!timeFormat) {
+    timeFormat = '24'
+  }
+  if (!locale) {
+    locale = 'en'
+  }
   if (!format) {
     format = {
-      month: 'MMM'
-    };
+      month: 'MMM',
+    }
   }
-  var start = moment(startDate).locale(locale).tz(timeZone);
+
+  let formatResult
+
+  const start = moment(startDate).locale(locale).tz(timeZone)
+
   if (endDate) {
-    var end = moment(endDate).locale(locale).tz(timeZone);
-    if (start.isSame(end,'month')) {
+    const end = moment(endDate).locale(locale).tz(timeZone)
+    if (sameMonth(start, end)) {
       if (sameDayOrNight(start, end)) {
         if (timeFormat && timeFormat === '12') {
-          return start.format('D '+format.month+' hh:mm A') + ' - ' + end.format('hh:mm A');
+          formatResult = start.format('D ' + format.month + ' hh:mm A') + ' - ' + end.format('hh:mm A')
         } else {
-          return start.format('D '+format.month+' HH:mm') + ' - ' + end.format('HH:mm');
+          formatResult = start.format('D ' + format.month + ' HH:mm') + ' - ' + end.format('HH:mm')
         }
       } else {
-        return start.format('D') + ' - ' + end.format('D '+format.month+'');
+        formatResult = start.format('D') + ' - ' + end.format('D ' + format.month + '')
       }
     } else {
-      return start.format('D '+format.month+'') + ' - ' + end.format('D '+format.month+'');
+      formatResult = start.format('D ' + format.month + '') + ' - ' + end.format('D ' + format.month + '')
     }
   } else {
     if (timeFormat && timeFormat === '12') {
-      return start.format('D '+format.month+' hh:mm A');
+      formatResult = start.format('D ' + format.month + ' hh:mm A')
     } else {
-      return start.format('D '+format.month+' HH:mm');
+      formatResult = start.format('D ' + format.month + ' HH:mm')
     }
   }
-};
+
+  if (showTimeZone) {
+    formatResult += ' ' + moment.tz(timeZone).format('z')
+  }
+
+  return formatResult
+}
--- test/index.js
+++ test/index.js
@@ -1,101 +1,114 @@
-var moment = require('moment-timezone');
+const moment = require('moment-timezone')
 
-var expect = require('chai').expect;
-var prettyDates = require('../src/');
+const { expect } = require('chai')
+const prettyDates = require('../src/')
 
-describe('Pretty dates', function() {
-
-  it('should handle AM same day', function() {
-    var dates = prettyDates({
-      startDate: moment('2015-02-08 09:30:00').toDate(),
-      endDate: moment('2015-02-08 10:30:00').toDate(),
+describe('Pretty dates', function () {
+  it('should handle AM same day', function () {
+    const dates = prettyDates({
+      startDate: moment.tz('2015-02-08 09:30:00', 'Europe/Berlin').toDate(),
+      endDate: moment.tz('2015-02-08 10:30:00', 'Europe/Berlin').toDate(),
       timeZone: 'Europe/Berlin',
-      timeFormat: '12'
-    });
-    expect(dates).to.equal('8 Feb 09:30 AM - 10:30 AM');
-  });
+      timeFormat: '12',
+    })
+    expect(dates).to.equal('8 Feb 09:30 AM - 10:30 AM')
+  })
 
-  it('should handle AM on two days', function() {
-    var dates = prettyDates({
-      startDate: moment('2015-02-08 09:30:00').toDate(),
-      endDate: moment('2015-02-09 10:30:00').toDate(),
+  it('should handle AM on two days', function () {
+    const dates = prettyDates({
+      startDate: moment.tz('2015-02-08 09:30:00', 'Europe/Berlin').toDate(),
+      endDate: moment.tz('2015-02-09 10:30:00', 'Europe/Berlin').toDate(),
       timeZone: 'Europe/Berlin',
-      timeFormat: '12'
-    });
-    expect(dates).to.equal('8 - 9 Feb');
-  });
+      timeFormat: '12',
+    })
+    expect(dates).to.equal('8 - 9 Feb')
+  })
 
-  it('should handle dates that start on same week day but different weeks', function() {
-    var dates = prettyDates({
-      startDate: moment('2015-07-03 10:30:00').toDate(),
-      endDate: moment('2015-07-10 10:30:00').toDate(),
+  it('should handle dates that start on same week day but different weeks', function () {
+    const dates = prettyDates({
+      startDate: moment.tz('2015-07-03 10:30:00', 'Europe/Berlin').toDate(),
+      endDate: moment.tz('2015-07-10 10:30:00', 'Europe/Berlin').toDate(),
       timeZone: 'Europe/Berlin',
-      timeFormat: '24'
-    });
-    expect(dates).to.equal('3 - 10 Jul');
-  });
+      timeFormat: '24',
+    })
+    expect(dates).to.equal('3 - 10 Jul')
+  })
 
-  it('should handle 24 hours same day', function() {
-    var dates = prettyDates({
-      startDate: moment('2015-02-08 09:30:00').toDate(),
-      endDate: moment('2015-02-08 10:30:00').toDate(),
+  it('should handle 24 hours same day', function () {
+    const dates = prettyDates({
+      startDate: moment.tz('2015-02-08 09:30:00', 'Europe/Berlin').toDate(),
+      endDate: moment.tz('2015-02-08 10:30:00', 'Europe/Berlin').toDate(),
       timeZone: 'Europe/Berlin',
-      timeFormat: '24'
-    });
-    expect(dates).to.equal('8 Feb 09:30 - 10:30');
-  });
+      timeFormat: '24',
+    })
+    expect(dates).to.equal('8 Feb 09:30 - 10:30')
+  })
 
-  it('should handle events that end late', function() {
-    var dates = prettyDates({
-      startDate: moment('2015-02-08 21:00:00').toDate(),
-      endDate: moment('2015-02-09 03:00:00').toDate(),
+  it('should handle events that end late', function () {
+    const dates = prettyDates({
+      startDate: moment.tz('2015-02-08 21:00:00', 'Europe/Berlin').toDate(),
+      endDate: moment.tz('2015-02-09 03:00:00', 'Europe/Berlin').toDate(),
       timeZone: 'Europe/Berlin',
-      timeFormat: '24'
-    });
-    expect(dates).to.equal('8 Feb 21:00 - 03:00');
-  });
+      timeFormat: '24',
+    })
+    expect(dates).to.equal('8 Feb 21:00 - 03:00')
+  })
 
-  it('should handle single day events with 24h clock', function() {
-    var dates = prettyDates({
-      startDate: moment('2015-02-08 21:00:00').toDate(),
+  it('should handle single day events with 24h clock', function () {
+    const dates = prettyDates({
+      startDate: moment.tz('2015-02-08 21:00:00', 'Europe/Berlin').toDate(),
       endDate: null,
       timeZone: 'Europe/Berlin',
-      timeFormat: '24'
-    });
-    expect(dates).to.equal('8 Feb 21:00');
-  });
+      timeFormat: '24',
+    })
+    expect(dates).to.equal('8 Feb 21:00')
+  })
 
-  it('should handle single day events with 12h clock', function() {
-    var dates = prettyDates({
-      startDate: moment('2015-02-08 21:00:00').toDate(),
+  it('should handle single day events with 12h clock', function () {
+    const dates = prettyDates({
+      startDate: moment.tz('2015-02-08 21:00:00', 'Europe/Berlin').toDate(),
       endDate: null,
       timeZone: 'Europe/Berlin',
-      timeFormat: '12'
-    });
-    expect(dates).to.equal('8 Feb 09:00 PM');
-  });
+      timeFormat: '12',
+    })
+    expect(dates).to.equal('8 Feb 09:00 PM')
+  })
 
-  it('should handle locales', function() {
-    var dates = prettyDates({
-      startDate: moment('2015-02-08 21:00:00').toDate(),
-      locale: 'en'
-    });
-    expect(dates).to.equal('8 Feb 21:00');
+  it('should handle locales', function () {
+    const dateEN = prettyDates({
+      startDate: moment.tz('2015-02-08 21:00:00', 'Europe/Berlin').toDate(),
+      locale: 'en',
+    })
+    expect(dateEN).to.equal('8 Feb 21:00')
 
-    var dates = prettyDates({
-      startDate: moment('2015-02-08 21:00:00').toDate(),
-      locale: 'sv'
-    });
-    expect(dates).to.equal('8 feb 21:00');
-  });
+    const dateSV = prettyDates({
+      startDate: moment.tz('2015-02-08 21:00:00', 'Europe/Berlin').toDate(),
+      locale: 'sv',
+    })
+    expect(dateSV).to.equal('8 feb 21:00')
+  })
 
-  it('should handle custom month format', function() {
-    var dates = prettyDates({
-      startDate: moment('2015-02-08 09:30:00').toDate(),
-      endDate: moment('2015-02-09 10:30:00').toDate(),
-      timeZone: 'Europe/Berlin',
-      timeFormat: '12'
-    }, { month: 'MMMM' });
-    expect(dates).to.equal('8 - 9 February');
-  });
-});
+  it('should handle custom month format', function () {
+    const dates = prettyDates(
+      {
+        startDate: moment.tz('2015-02-08 09:30:00', 'Europe/Berlin').toDate(),
+        endDate: moment.tz('2015-02-09 10:30:00', 'Europe/Berlin').toDate(),
+        timeZone: 'Europe/Berlin',
+        timeFormat: '12',
+      },
+      { month: 'MMMM' }
+    )
+    expect(dates).to.equal('8 - 9 February')
+  })
+
+  it('should show time zone', function () {
+    const dates = prettyDates({
+      startDate: moment.tz('2015-02-09 09:30:00', 'America/New_York').toDate(),
+      endDate: moment.tz('2015-02-09 10:30:00', 'America/New_York').toDate(),
+      timeZone: 'America/New_York',
+      timeFormat: '24',
+      showTimeZone: true,
+    })
+    expect(dates).to.equal('9 Feb 09:30 - 10:30 EST')
+  })
+})
