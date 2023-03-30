05caca1a804088e3dc8139a537063719fd1a9e91
Jonny Strömberg <jonny.stromberg@gmail.com>
2023-03-30 10:12:44 +0200

    Show year if other than current

--- src/index.js
+++ src/index.js
@@ -22,28 +22,42 @@ module.exports = function ({ endDate, startDate, timeZone, timeFormat, locale, s
   let formatResult
 
   const start = moment(startDate).locale(locale).tz(timeZone)
+  const isCurrentYear = start.year() === moment().year()
 
   if (endDate) {
     const end = moment(endDate).locale(locale).tz(timeZone)
 
     if (sameDayOrNight(start, end)) {
       if (timeFormat && timeFormat === '12') {
-        formatResult = `${start.format(`D ${format.month} hh:mmA`)}-${end.format('hh:mmA')}`
+        formatResult = `${start.format(`D ${format.month} hh:mmA`)}-${end.format('hh:mmA')}${
+          isCurrentYear ? '' : end.format(' YYYY')
+        }`
       } else {
-        formatResult = `${start.format(`D ${format.month} HH:mm`)}-${end.format('HH:mm')}`
+        formatResult = `${start.format(`D ${format.month}`)}${
+          isCurrentYear ? '' : end.format(' YYYY -')
+        } ${start.format('HH:mm')}-${end.format('HH:mm')}`
       }
     } else {
       if (sameMonth(start, end)) {
-        formatResult = `${start.format('D')} - ${end.format(`D ${format.month}`)}`
+        formatResult = `${start.format('D')}-${end.format(`D ${format.month}`)}${
+          isCurrentYear ? '' : end.format(' YYYY')
+        }`
       } else {
-        formatResult = start.format(`D ${format.month}`) + ' - ' + end.format(`D ${format.month}`)
+        formatResult =
+          start.format(`D ${format.month}`) +
+          ' - ' +
+          end.format(`D ${format.month}${isCurrentYear ? '' : end.format(' YYYY')}`)
       }
     }
   } else {
     if (timeFormat && timeFormat === '12') {
-      formatResult = start.format(`D ${format.month} hh:mm A`)
+      formatResult = `${start.format(`D ${format.month}`)}${
+        isCurrentYear ? '' : start.format(' YYYY -')
+      } ${start.format(`hh:mm A`)}`
     } else {
-      formatResult = start.format(`D ${format.month} HH:mm`)
+      formatResult = `${start.format(`D ${format.month}`)}${
+        isCurrentYear ? '' : start.format(' YYYY -')
+      } ${start.format(`HH:mm`)}`
     }
   }
 
